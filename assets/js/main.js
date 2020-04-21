76d80e5d9882961f5d2b0dd9d86ddc2443d0c088
heshamabdulzaher <heshamabdulzaher@gmail.com>
2020-04-21 10:27:37 +0200

    Update mobile view styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,7 @@ if (window.location.pathname.includes('/article')) {
   let scrollProgressPercentage = 0;
   let pageHeight = document.body.scrollHeight - window.innerHeight;
   document.addEventListener('scroll', function (e) {
-    scrollProgressPercentage =
-      Math.round((window.scrollY * 100) / pageHeight) / 100;
+    scrollProgressPercentage = (window.scrollY * 100) / pageHeight / 100;
     progressBar.style.transform = `scaleX(${scrollProgressPercentage})`;
     if (window.scrollY >= 400) {
       header.classList.add('show');
